91ad1978d47abda1046fd5b08ba7f5478c399774
Robin L. Fuller <robinl.fuller@Robins-MacBook-Pro.local>
2022-05-13 11:54:13 -0500

    aria-current

--- navbar.js
+++ navbar.js
@@ -9,22 +9,22 @@ function NavBar(){
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#/CreateAccount/">Create Account</a>
+              <a className="nav-link" aria-current="page" href="#/CreateAccount/">Create Account</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#/login/">Login</a>
+              <a className="nav-link" aria-current="page" href="#/login/">Login</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#/deposit/">Deposit</a>
+              <a className="nav-link" aria-current="page" href="#/deposit/">Deposit</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#/withdraw/">Withdraw</a>
+              <a className="nav-link" aria-current="page" href="#/withdraw/">Withdraw</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#/balance/">Balance</a>
+              <a className="nav-link" aria-current="page" href="#/balance/">Balance</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#/alldata/">AllData</a>
+              <a className="nav-link" aria-current="page" href="#/alldata/">AllData</a>
             </li>          
           </ul>
         </div>
