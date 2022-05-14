359508aac03cf7a73f625a1bcb06c3c7a1026f42
Robin L. Fuller <robinl.fuller@Robins-MacBook-Pro.local>
2022-05-13 19:01:21 -0500

    revising code for navbar

--- navbar.js
+++ navbar.js
@@ -1,10 +1,3 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
-  if(link.href.includes(`${activePage}`)) {
-    link.classList.add(`active`);
-  }
-})
-
 function NavBar(){
     return(
       <>
