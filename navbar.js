e68ae68d98029b4882ec24bf98738c2c3a75bccd
Robin L. Fuller <robinl.fuller@Robins-MacBook-Pro.local>
2022-05-15 17:14:04 -0500

    js navbar highlight

--- navbar.js
+++ navbar.js
@@ -1,3 +1,6 @@
+
+
+
 function NavBar(){
     return(
       <>
@@ -32,10 +35,3 @@ function NavBar(){
       </>
     );
   }
-
-  const activePage = window.location.pathname;
-  const navLinks = document.querySelectorAll('nav a').forEach(link => {
-      if(link.href.includes(`${activePage}`)) {
-        link.classList.add(`active`);
-  }
-})
