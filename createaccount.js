6d848986bc1658912ec47b734d0e233a11123c6b
Robin L. Fuller <robinl.fuller@Robins-MacBook-Pro.local>
2022-05-15 19:08:46 -0500

    grammar correction

--- createaccount.js
+++ createaccount.js
@@ -47,7 +47,7 @@ function CreateAccount(){
               <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)} /><br/>
               Password<br/>
               <input type="input" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)} /><br/>
-              <button type="submit" className="btn btn-light" onClick= {handleCreate}>Creat Account</button>
+              <button type="submit" className="btn btn-light" onClick= {handleCreate}>Create Account</button>
               </>
           ):(
               <>
