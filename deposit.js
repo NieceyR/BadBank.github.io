9afb502dd57e098ca5d971b24dabcaaf376b3114
Robin L. Fuller <robinl.fuller@Robins-MacBook-Pro.local>
2022-05-16 17:54:39 -0500

    deposit card

--- deposit.js
+++ deposit.js
@@ -1,8 +1,14 @@
 function Deposit(){
     const ctx = React.useContext(UserContext);
-    return (
+    return ( 
       <>
-      <h5>Deposit</h5>
+        <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Deposit</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Deposit</a>
+      </div>
+    </div>
       {JSON.stringify(ctx)}<br/>
       </>
     );
