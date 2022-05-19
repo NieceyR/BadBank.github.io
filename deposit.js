bcaf32696029eb1db99b9c250643a3b4762ef1ca
Robin L. Fuller <robinl.fuller@Robins-MacBook-Pro.local>
2022-05-19 10:57:24 -0500

    deposit page

--- deposit.js
+++ deposit.js
@@ -1,53 +1,125 @@
 function Deposit(){
     const [show, setShow] = React.useState(true);
     const [status, setStatus] = React.useState(' ');
+    const [user, setUser] = React.useState('');
+    const [loginEmail, setLoginEmail] = React.useState('');
+    const [loginPassword, setLoginPassword] = React.useState('');
+    const [balance, setBalance] = React.useState('');
+    const [deposit, setDeposit] = React.useState('');
+    const [isValid, setIsValid] = React.useState(false);
+    const ctx = React.useContect(UserContext);
+    const [buttonStatus, setButtonStatus] = React.useState(true);
 
-    return ( 
+    if (show) {
+      for (const {name, email, password, balance, loggedin} of ctx.users) {
+        if (loggedin) {
+          setShow(false);
+          setUser(name);
+          setLoginEmail(email);
+          setLoginPassword(password);
+          setBalance(balance);
+          return
+        }
+      }
+    }
+
+    function handleChange(e) {
+      if (e.target.value === null) {
+        setButtonStatus(true)
+        setDeposit(e.currentTarget.value)
+      } else {
+        setButtonStatus(false)
+        setDeposit(e.currentTarget.value)
+      }
+    }
+
+    function HandleDeposit() {
+      if (!isNaN(deposit) && deposit > 0) {
+        let newBalance = Number(balance) + Number(deposit);
+        let tracker = false;
+        setBalance(newBalance);
+        setDeposit('');
+        setStatus(`$${deposit} has been successfully deposited into your account`);
+        setTimeout(() => setStatus(''), 6000);
+
+        for (const {email, password, balance} of ctx.users) {
+          if (loginEmail === email && loginPassword === password) {
+            for (var i = 0, length = ctx.users.length; i < length; i++) {
+              if (ctx.users[i].email === loginEmail) {
+                ctx.users[i].balance === Number(newBalance);
+
+                tracker = true;
+              }
+            }
+          }
+        }
+        if (tracker) {
+          setStatus(`$${deposit} deposited into account`);
+          setTimeout(() => setStatus(''), 5000);
+          setDeposit('');
+          setBalance(Number(newBalance));
+          setButtonStatus(true);
+        }
+      } else if (!isNaN(deposit)) {
+        setStatus('Transaction failed. Amount must be greater than $0.00.');
+        setDeposit('');
+        setTimeout(() => setStatus(''), 5000);
+        setButtonStatus(true);
+        alert(`Error: deposit amount must be greater than $0.00`);
+      } else {
+        setStatus('Transaction failed. Deposit amount must be a numerical value.');
+        setDeposit('');
+        setTimeout(() => setStatus(''), 5000);
+        setButtonStatus(true);
+        alert(`Error: deposit amount must be a numerical value`);
+      }
+      return;
+    }
+
+    return (
       <Card
-      bgcolor= "secondary"
-      header="Deposit"
-      status={status}
-      body={show ?
-      <DepositForm setShow={setShow}/> :
-      <DepositMsg setShow={setShow}/>}
+        bgcolor="secondary"
+        header="Deposit"
+        status={status}
+        body={
+          show ? (
+            <> 
+
+            You must <a href="#/login/" className="btnDeposit" data-toggle="tooltip" title= "Login to your account"></a> {' '} to make a transaction.
+            <br/>
+            <br/>
+            </>
+          ) : (
+            <>
+            <h4>Current Balance: ${balance}</h4>
+            <br/>
+            Deposit Amount
+            <br/>
+            <input 
+              type="input"
+              className="form-control" 
+              id="deposit"
+              placeholder="$0.00"
+              value={deposit}
+              onChange={handleChange}
+              />
+              <br/>
+              <>
+              <button
+              id="button"
+              type="submit"
+              className="btn btn-light"
+              disabled={buttonStatus}
+              onClick={HandleDeposit}
+              >Deposit</button>
+              <br/>
+              <br/>
+              <div className="text-left"></div>
+              </>
+              </>
+
+          )
+        }
       />
-     
     );
   }
-
-  function DepositMsg(props) {
-    return(<>
-    <h5>Deposit Amount</h5>
-    <button type="submit" className="btn btn-light" onClick={() => props.setShow(true)}>Success!</button>
-    
-    </>);
-  }
-
-  function DepositForm(props) {
-    const [email, setEmail] = React.useState('');
-    const [amount, setAmount] = React.useState('');
-
-    function handle(){
-      console.log(email, amount);
-      const url = `/account/deposit/${email}/${amount}`;
-      (async () => {
-        var res = await fetch(url);
-        var data = await res.json();
-        console.log(data);
-      })();
-      props.setShow(false);
-    }
-    return(<>
-    
-    Email address<br/>
-    <input type="input" className="form-control" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    Amount<br/>
-    <input type="number" className="form-control" placeholder="Enter amount" value={amount} onChange={e => setAmount(e.currentTarget.value)}/><br/>
-
-    <button type="submit" className="btn btn-light" onClick={handle}>Deposit</button>
-    </>);
-
-
-  }
-  
